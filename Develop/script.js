57845f138d9bf1a14549c51f5167b29d0369ba2e
Jake Zook <jakezook01@gmail.com>
2023-09-01 14:04:24 -0600

    Adjusted HTML to show all hours of the work day.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var calendarDisplay = document.querySelector("#calendar");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
