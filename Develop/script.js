b502061cc63d78cec5b25ac301ce71c7cfd70771
Jake Zook <jakezook01@gmail.com>
2023-09-01 19:24:56 -0600

    Added logic to get the value from the text box and save it in local memory when the submit button inside the element is clicked.

--- Develop/script.js
+++ Develop/script.js
@@ -3,23 +3,37 @@
 // in the html.
 var calendarDisplay = document.querySelector("#calendar");
 
-$(function () {
+var saveButtons = document.querySelectorAll(".saveBtn");
+
+
+function handleSubmit() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  var parentID = $(this).parent().attr('id');
+  console.log(parentID);
+  var descriptionBox = $('#' + parentID).children(".description");
+  var description = descriptionBox.val()
+
+  localStorage.setItem("Description-" + parentID, description);
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+    // console.log(calendarDisplay.children[0].children[1].textContent);
   // TODO: Add code to display the current date in the header of the page.
-});
+}
+
+for (let i = 0; i < saveButtons.length; i++)
+{
+  saveButtons[i].addEventListener('click', handleSubmit);
+}
