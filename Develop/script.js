73a79e25396328604652aca79f43e83f70805fd7
Jake Zook <jakezook01@gmail.com>
2023-09-01 20:38:58 -0600

    Added logic to change color of time block depoending on the current time.

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,56 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var currentDayDisplay = document.querySelector("#currentDay");
 var calendarDisplay = document.querySelector("#calendar");
-
+var timeBlocks = document.querySelectorAll(".time-block");
 var saveButtons = document.querySelectorAll(".saveBtn");
 
+var date = dayjs();
+var currentHour = dayjs().hour();
+
+$(currentDayDisplay).text(date.format("MMMM D, YYYY"));
+
+setCalendarDisplay();
 
 function handleSubmit() {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  var parent = $(this).parent();
   var parentID = $(this).parent().attr('id');
-  console.log(parentID);
   var descriptionBox = $('#' + parentID).children(".description");
   var description = descriptionBox.val()
 
   localStorage.setItem("Description-" + parentID, description);
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   
+  setCalendarDisplay();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
     // console.log(calendarDisplay.children[0].children[1].textContent);
   // TODO: Add code to display the current date in the header of the page.
+}
+
+function setCalendarDisplay()
+{
+  for (let i = 0; i < timeBlocks.length; i++)
+  {
+    var timeBlock = $(timeBlocks[i]);
+    var timeBlockID = $(timeBlocks[i]).attr('id');
+    var hour = parseInt(timeBlockID);
+
+    var test = 13;
+
+    if (test > hour)
+    {
+      timeBlock.addClass('past');
+    }
+    else if (test === hour)
+    {
+      timeBlock.addClass('present');
+    }
+    else
+    {
+      timeBlock.addClass('future');
+    }
+  }
+
 }
 
 for (let i = 0; i < saveButtons.length; i++)
